7d4b81e277400c4c0ec66ad6197427ff07b6faa3
Talha-TN <nawazt964@gmail.com>
2024-10-31 13:50:04 -0700

    add error and loading files inside routes

---
+++ src/app/products/errors.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const errors = ({error }) => {
+  return (
+    <div>the custom errors {error.message}</div>
+  )
+}
+
+export default errors
---
+++ src/app/products/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <h1>loading... please wait</h1>
+  )
+}
+
+export default loading
