3cf44743073a32e7c6cc53e3a779efdb24178148
talha-fullstackdev <talhafullstack786@gmail.com>
2025-04-25 15:47:53 +0500

    made put api in next js and updated employee data using postman to test if api is working or not

--- src/app/api/db-emp/[empID]/route.js
+++ src/app/api/db-emp/[empID]/route.js
@@ -1,9 +1,10 @@
-import mongoose from "mongoose"
-import { connectionString } from "@/lib/database/db_connection"
-import { Employee } from "@/lib/database/model/employee"
-import { NextResponse } from "next/server"
+import mongoose from "mongoose";
+import { connectionString } from "@/lib/database/db_connection";
+import { Employee } from "@/lib/database/model/employee";
+import { NextResponse } from "next/server";
 
-export const DELETE = async (req,{ params }) => { // here we have to pass req parameter if it is in used or not
+export const DELETE = async (req, { params }) => {
+  // here we have to pass req parameter if it is in used or not
   try {
     await mongoose.connect(connectionString);
     const { empID } = params;
@@ -20,22 +21,37 @@ export const DELETE = async (req,{ params }) => { // here we have to pass req pa
     );
   } catch (err) {
     console.error("Server side error", err);
-    return NextResponse.json(
-      { msg: "Server side error" },
-      { status: 500 }
-    );
+    return NextResponse.json({ msg: "Server side error" }, { status: 500 });
   }
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////// put api for updating data
-export const PUT = async(req,{params})=>{
-  try{
-    await mongoose.connect(connectionString)
-    const {empID}=params
-
-  }catch(err){
-    console.err("Server side error occured while updating employee data")
-   return NextResponse.json({msg:"Server side error"},{status:500})
+export const PUT = async (req, { params }) => {
+  try {
+    await mongoose.connect(connectionString);
+    const body = await req.json();
+    const { name, email, gender, department, position } = body;
+    const { empID } = params;
+    // const id = { _id: empID }; // we can also use id like this
+    let updatedEmp = await Employee.findByIdAndUpdate(
+      // id
+       empID,
+      { name, email, gender, department, position },
+      { new: true }
+    );
+    if (!updatedEmp) {
+      return NextResponse.json(
+        { msg: "No employee found with this id" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(
+      { msg: "updated succesfully", result: updatedEmp },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.err("Server side error occured while updating employee data");
+    return NextResponse.json({ msg: "Server side error" }, { status: 500 });
   }
-}
+};
