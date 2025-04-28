f175db469db7075d97c34f85f88ccbf96c2b5011
talha-fullstackdev <talhafullstack786@gmail.com>
2025-04-28 17:59:27 +0500

    made a simple file uploading frontend

--- src/app/api/upload-files/route.js
+++ src/app/api/upload-files/route.js
@@ -2,9 +2,10 @@ import { NextResponse } from "next/server";
 
 export const GET = async (req, res) => {
   try {
-    
+
   } catch (err) {
     console.error("error", err);
     return NextResponse.json({ msg: "server side error" }, { status: 500 });
   }
 };
+////
