eb5a8d8da0141abfcd7274966b4dbe67d6b26bf7
talha-fullstackdev <talhafullstack786@gmail.com>
2025-04-29 17:33:13 +0500

    made api for posting files and images to data base

--- src/app/api/upload-mongo-image/route.js
+++ src/app/api/upload-mongo-image/route.js
@@ -1,11 +1,33 @@
-import { NextResponse } from "next/server"
-
-export const POST = async (req,res) =>{
-    try{
-        return NextResponse.json({msg:"hello talha from mongo db image upload",success:true},{status:200})
-
-    }catch(err){
-        console.error("error occur >>>> ",err)
-        return NextResponse.json({msg:"server side error!",success : false},{status:500})
+import { NextResponse } from "next/server";
+import { connectionString } from "@/lib/database/db_connection";
+import mongoose from "mongoose";
+import { Image } from "@/lib/database/model/image";
+export const POST = async (req, res) => {
+  try {
+    await mongoose.connect(connectionString);
+    const data = await req.formData()
+    const file = data.get("file")
+    if(!file){
+        return NextResponse.json({msg:"No file found!",success:false},{status:404})
     }
-}
+    const bufferData = await file.arrayBuffer()
+    const buffer = Buffer.from(bufferData)
+    const newImage = new Image({
+        name:file.name,
+        data:buffer,
+        contentType:file.type
+    })
+    await newImage.save()
+    return NextResponse.json(
+      { msg: "file uploaded succesfully🎊", success: true },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error("error occur >>>> ", err);
+    console.log("error occur >>>> ", err);
+    return NextResponse.json(
+      { msg: "server side error!", success: false },
+      { status: 500 }
+    );
+  }
+};
