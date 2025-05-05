3afd0caa2631fef09957813b61df802eb224bb60
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-05 18:07:35 +0500

    formated code

--- src/app/f1/(.)f2/page.js
+++ src/app/f1/(.)f2/page.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const F2Dot = () => {
-  return (
-    <div>page f2Dot</div>
-  )
-}
+  return <div>page f2Dot</div>;
+};
 
-export default F2Dot
+export default F2Dot;
