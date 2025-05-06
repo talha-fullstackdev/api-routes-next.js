24285722065e0542064bc95ce8313dff478a69e7
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-06 11:08:39 +0500

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 // import React from 'react'
 // import Link from 'next/link'
 // import { usePathname } from 'next/navigation'
@@ -17,38 +17,40 @@
 
 //      </ul>
 //     </header>
-    
+
 //   )
 // }
 // export default Header
 ////////////////////////////
-import React from 'react';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 const Header = () => {
   const pathName = usePathname();
   const links = [
-    { name: 'About', href: '/about' },
-    { name: 'Contact', href: '/contact' },
-    { name: 'Profile', href: '/profile' },
-    { name: 'Docs', href: '/docs' },
-    { name: 'User One', href: '/user-one' },
-    { name: 'products', href: '/products' }
+    { name: "About", href: "/about" },
+    { name: "Contact", href: "/contact" },
+    { name: "Profile", href: "/profile" },
+    { name: "Docs", href: "/docs" },
+    { name: "User One", href: "/user-one" },
+    { name: "products", href: "/products" },
   ];
   return (
     <header className="bg-slate-600 p-4">
       <h1 className="text-center font-semibold text-2xl text-white">Header</h1>
       <ul className="flex gap-4">
         {links.map((link) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className={
-                pathName === link.href ? 'bg-red-700 px-2 rounded-md text-white' : ''
-              }
-            >
-              {link.name}
-            </Link>
+          <Link
+            key={link.href}
+            href={link.href}
+            className={
+              pathName === link.href
+                ? "bg-red-700 px-2 rounded-md text-white"
+                : ""
+            }
+          >
+            {link.name}
+          </Link>
         ))}
       </ul>
     </header>
