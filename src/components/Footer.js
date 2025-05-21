f50e1dcff0c78f7c4b4a21d0caef8a97bc1d2b06
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-21 10:58:06 +0500

    updated and formated footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 const Footer = () => {
   return (
-    <footer className='bg-slate-600 p-4' >
-        <h1 className='text-center text-2xl font-semibold text-white'>Footer</h1>
+    <footer className="bg-slate-600 p-4">
+      <h1 className="text-center text-2xl font-semibold text-white">Footer</h1>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
