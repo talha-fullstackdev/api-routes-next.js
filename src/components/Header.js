85d2b1e316ad0f2ad4723c69d97f70a9c599c5a7
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-21 10:59:04 +0500

    remove extra commented code from header componnet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,4 @@
 "use client";
-// import React from 'react'
-// import Link from 'next/link'
-// import { usePathname } from 'next/navigation'
-// const Header = () => {
-//   const pathName=usePathname()
-//   const isActive = pathName.startsWith(Link.href)
-//   return (
-//     <header className='bg-slate-600 p-4 '>
-//      <h1 className='text-center font-semibold text-2xl text-white'>Header</h1>
-//      <ul className='flex gap-4'>
-//       <Link key={Link.name} className={isActive?"font-semibold text-red":""} href="/about">about</Link>
-//       <Link key={Link.name} className={isActive?"font-semibold text-red":""} href="/contact">contact</Link>
-//       <Link key={Link.name} className={isActive?"font-semibold text-red":""} href="/profile">profile</Link>
-//       <Link key={Link.name} className={isActive?"font-semibold text-red":""} href="/docs">docs</Link>
-//       <Link key={Link.name} className={isActive?"font-semibold text-red":""} href="/user-one">user one</Link>
-
-//      </ul>
-//     </header>
-
-//   )
-// }
-// export default Header
-////////////////////////////
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
